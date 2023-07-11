9649338c0c09139a486fb79deeb9638891bf0ae9
jaearl <jared.s.earl123@gmail.com>
2023-07-11 17:04:38 -0500

    reconfigure deploy script to not mess with the nginx config cause certbot makes automatic changes to the config

--- deploy_site.sh
+++ deploy_site.sh
@@ -16,10 +16,11 @@ fi
 
 ng build --prod
 
-cp ./deploy/jared-website.conf /etc/nginx/sites-available/
-ls -s /etc/nginx/sites-available/jared-website.conf /etc/nginx/sites-enabled/
+#cp ./deploy/jared-website.conf /etc/nginx/sites-available/
+#ls -s /etc/nginx/sites-available/jared-website.conf /etc/nginx/sites-enabled/
 
-cp -r dist/jared-website /srv/
+rm -rf /var/www/jared-earl.com
+cp -r dist/jared-website /var/www/jared-earl.com
 
 systemctl restart nginx
 systemctl status nginx
